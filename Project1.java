1632cb6cbca9d546b6a5da1c939fb86c8c35abe5
Marv0192 <marvintovar92@gmail.com>
2022-03-18 09:49:23 -0700

    completed project1

--- Project1.java
+++ Project1.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Project1 {
@@ -8,53 +9,75 @@ public class Project1 {
 	public static void main(String[] args) throws FileNotFoundException {
 		File testFile = new File("Example.txt");
 		Scanner scr = new Scanner(testFile);
-		
-		String state = "";
-		String emptyPSet = "";
-		
-		ArrayList<String> transitions = new ArrayList<>();
-		
+		String emptySubSet = "";
+		HashMap<String, String> stateTransitionMap = new HashMap<>();
 		int startingIndex = -1;
 		
 		while(scr.hasNext()) {
 			String line = scr.nextLine();
-			String split[] = line.split(",", 2); //split the line into the states and transitions
-			state += split[0]; //add the states to the state array list
+			String[] split = line.split(",", 2); //split the line into the states and transitions
 			
 			if(split[1].equals("empty")) {
-				transitions.add(null); //if transition element is "empty" then put null
+				stateTransitionMap.put(split[0], ""); //if transition is empty add state as the key 
+														//and "" as the value to h-map
 			}
 			else {
-				ArrayList<Character> temp = new ArrayList<>();
+				String temp = "";
 				for(int i = 0; i < split[1].length(); i++) {
-					if(Character.isDigit(split[1].charAt(i))) {
-						temp.add(split[1].charAt(i));
+					if(Character.isDigit(split[1].charAt(i))) { 		//if the character in transition is a digit build a string of all
+						temp += Character.toString(split[1].charAt(i));	//the digits	
+						
 					}
 				}
-				transitions.add(temp.toString()); //if transition element is a digit then add it to transition array list
+				stateTransitionMap.put(split[0], temp); //add the state as the key and the string of digits as the value
 			}
 		}
+		scr.close();
 		
-		findPowerSet(state, emptyPSet, startingIndex);
+		//print out all the states(keys) and transitions(values)
+		for(Entry<String, String> e : stateTransitionMap.entrySet()) {
+			System.out.println("State:" + e.getKey() + " Transitions: " + e.getValue());
+		}
+		
+		//create an array of all the states and pass to findPowerSet()
+		Object[] states = stateTransitionMap.keySet().toArray();
+		System.out.print("\nPower Set: { ");
+		findPowerSet(states, emptySubSet, startingIndex);
+		System.out.print("}\n");
+		
+		for(Entry<String, String> e : stateTransitionMap.entrySet()) {
+			System.out.print("E(" + e.getKey() + ") = " + e.getKey());
+			E(stateTransitionMap, e.getKey());
+		}
 
 	}
 	
-	static void findPowerSet(String states, String pSet, int start) {
-		int length = states.length();
+	private static void E(HashMap<String, String> stateTransitionMap, String key) {
+		String currTransition = stateTransitionMap.get(key);
+		if(currTransition.isEmpty()) {
+			System.out.println();
+			return;
+		}
+		
+		System.out.print(currTransition);
+		key = Character.toString(currTransition.charAt(currTransition.length()-1));
+		E(stateTransitionMap, key);
+		
+	}
+
+	static void findPowerSet(Object[] states, String currSubSet, int start) {
+		int length = states.length;
 		
 		if (start == length) {
 			return;
 		}
 		
-		System.out.println(pSet);
+		System.out.print("{" + currSubSet + "} ");
 		
 		for(int i = start + 1; i < length; i++) {
-			pSet += states.charAt(i);
-			findPowerSet(states, pSet, i);
-			pSet = pSet.substring(0, pSet.length()- 1);
+			currSubSet += states[i];
+			findPowerSet(states, currSubSet, i);
+			currSubSet = currSubSet.substring(0, currSubSet.length()- 1);
 		}
 	}
-	
-	
-
 }
